16aee59fa5faea9bfa3708eb279ea530738d8d1b
Nishant Ranjan Verma <nishantranjanverma@gmail.com>
2024-07-31 22:36:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class LoanPrediction(BaseModel):
 
 @app.get('/')
 def index():
-    return {'Message':'Welcome to Loan Prediction App using API - CI CD Jenkins'}
+    return {'Message':'Welcome to Loan Prediction App using API - CI CT CD Jenkins V2'}
 
 @app.post('/prediction_api')
 def predict(loan_details: LoanPrediction):
